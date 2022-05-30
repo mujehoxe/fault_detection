d9238ce0132f0a32d06c14cb9c1fb11b2837f7e2
oussama gaham <oussama.gaham@univ-constantine2.dz>
2022-05-30 20:40:56 +0100

    json sender

--- sender.py
+++ sender.py
@@ -1,16 +1,27 @@
 import socket
-from typing import List
+import json
+import time
+
+from simulator import Simulator
 
 
 class Sender:
 
-    host = socket.gethostname()
-    port = 12345
+    addr = (socket.gethostname(), 12345)
 
-    def __init__(self) -> None:
+    def __init__(self, simulator: Simulator) -> None:
+        self.establish_connection()
+        simulator.add_observer(self)
+
+    def establish_connection(self):
         self.sock = socket.socket(
             socket.AF_INET, socket.SOCK_STREAM)
+        while self.sock.connect_ex(self.addr) != 0:
+            time.sleep(5)
 
-    def send(self, msg):
-        self.sock.connect((self.host, self.port))
-        self.sock.sendall(msg.encode())
+    def send(self, data):
+        try:
+            self.sock.sendall(bytes(json.dumps(data), encoding='utf-8'))
+        except socket.error:
+            self.sock.close()
+            self.establish_connection()
